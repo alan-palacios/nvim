ca34748a2c5ec024c6ae659737106b439b3bd8bb
alan-palacios <apyolt@gmail.com>
2023-06-29 13:35:02 -0600

    add lazy plugin manager

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require('mappings')
+require('plugins')
 
 opt = vim.opt
 
@@ -21,3 +22,19 @@ opt.smartindent = true
 -- wrap
 opt.wrap = true
 opt.breakindent = true
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
