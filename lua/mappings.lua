2cd0e78d871eb075fa589eef06cee645ad67a10d
alan-palacios <apyolt@gmail.com>
2023-07-03 22:39:08 -0600

    Update config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,3 +13,4 @@ end
 map("n", "<C-S>", ":update<CR>", { silent = true })
 map("v", "<C-S>", "<C-C>:update<CR>", { silent = true })
 map("i", "<C-S>", "<C-O>:update<CR>", { silent = true })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin"},
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.2', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   {
     "LazyVim/LazyVim",
     opts = {
